0e83516ee9c3da102bdeeae3d5be92f340dbe555
BasilioGN <basilio10.gn@gmail.com>
2025-03-12 18:36:03 +0000

    testing synths, pan and amp for the first time

--- Sonic_Pi_TFE_File_tests_1.rb
+++ Sonic_Pi_TFE_File_tests_1.rb
@@ -1,6 +1,26 @@
 # Welcome to Sonic Pi
-play :C3
-sleep 0.5
-play :D3
-sleep 0.5
-play :E4
+use_synth :pulse
+
+play 50, amp: 0.1, pan: -1
+sleep 0.25
+play 55, amp: 0.2, pan: 1
+sleep 0.25
+play 57, amp: 0.4, pan: -1
+sleep 0.25
+play 62, amp: 1, pan: 0
+sleep 0.25
+
+use_synth :prophet
+play 38
+sleep 0.25
+play 50
+sleep 0.25
+play 62
+sleep 0.25
+
+use_synth :tb303
+play 38
+use_synth :dsaw
+play 50
+use_synth :prophet
+play 57
