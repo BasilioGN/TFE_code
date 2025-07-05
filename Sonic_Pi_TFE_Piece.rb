9616ea1ef3e44b0eb54a838a06bb5984e335443e
BasilioGN <basilio10.gn@gmail.com>
2025-07-05 19:03:19 +0100

    defining functions

--- Sonic_Pi_TFE_Piece.rb
+++ Sonic_Pi_TFE_Piece.rb
@@ -6,28 +6,45 @@
 # - Link to repository: https://github.com/BasilioGN/TFE_code.git
 #################################################################
 
+#################################################################
+#
+#Title: if((this + S_P) == 1) {print "I'm an instrument!"}
+#
+#################################################################
+
 # Global config
 use_bpm 120 #indicates de bpm that the song will have globally.
 
+#--------- Functions declaration ----------
+def intro_pads(color, synth_to_use, duration, volume)
+  use_synth synth_to_use # ojo que puede petar aquí.
+  
+  if color == true
+    play_color_pad(duration, volume)
+  else
+    play_dissonant_pad(duration, volume)
+  end
+end
+
+
+def play_color_pad(pad_duration, volume, time_between_chords)
+  use_synth :sine 
+  play chord(:D3, :major), release: 2, amp: 0.5
+  sleep time_between_chords
+  play chord(:F2, :major), release: 2, amp: 0.5
+  sleep time_between_chords
+  play chord(:A2, :major), release: 2, amp: 0.5
+  sleep time_between_chords
+  play chord(:C3, :major), release: 2 , amp: 0.5
+  sleep time_between_chords
+end
+
+play_color_pad(2, 2, 2)
+
+#def play_dissonant_pad(pad_duration, volume)
+#end
+
+
+
 #--------- Section A -----------
 
-def play_legato_note(note_value, duration)
-  release_duration = duration
-  if duration < 1
-    release_duration = 1
-  end
-  
-  play note_value, release: release_duration, amp: 0.40
-  sleep duration
-end
-
-# arpeggio parts
-
-def arp_d_2
-  play_pattern_timed [:d4, :fs4, :b4, :cs5, :fs5, :fs4, :b4, :cs5], [0.5], amp: 0.5
-  play_pattern_timed [:e5, :fs4, :b4, :cs5, :fs5, :fs4, :b4, :cs5], [0.5], amp: 0.5
-end
-
-live_loop :play_melody do
-  arp_d_2
-end
