5124d9830f4bae32cb840258e71953c85b3744fb
BasilioGN <basilio10.gn@gmail.com>
2025-07-16 00:22:56 +0100

    deleted non used files

--- Sonic_Pi_TFE_File_tests_1.rb
+++
@@ -1,2 +0,0 @@
-# Welcome to Sonic Pi
-sample :loop_amen, rate: -1.5
--- Sonic_Pi_TFE_samples_test.rb
+++
@@ -1,6 +0,0 @@
-# Welcome to Sonic Pi
-print sample_duration :drum_cymbal_open
-#sample :loop_amen, rate: 2, attack: 0.01, sustain: 0, release: 0.35
-#sample "/Users/basiliogomeznavarro/Dropbox/AA_BASI/Samples/CongasBongos-DanceDrums/BONGO 1.wav", rate: 0.5, amp: 0.4
-samps = "/Users/basiliogomeznavarro/Dropbox/AA_BASI/Samples/CongasBongos-DanceDrums/"
-sample samps, 1
